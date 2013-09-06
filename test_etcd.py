d53fb3b015cbfd9f93ae901940270965eb012288
Kris Foster <kris.foster@gmail.com>
2013-09-06 01:05:30 -0700

    start of reworking the unittests

--- test_etcd.py
+++ test_etcd.py
@@ -4,36 +4,31 @@ Test suite for etcd-py
 """
 
 import sys
+import uuid
+import unittest
 
 from etcd import *
 
 
+class TestEtcd(unittest.TestCase):
+    def setUp(self):
+        self.e = etcd.Etcd("10.0.1.110")
+
+    def test_crud(self):
+        test_key = uuid.uuid4().hex
+        first_value = uuid.uuid4().hex
+        self.assertEqual(self.e.set(test_key, first_value).prevValue, None)
+        self.assertEqual(self.e.get(test_key).value, first_value)
+        second_value = uuid.uuid4().hex
+        self.assertEqual(self.e.set(test_key, second_value).prevValue,
+                first_value)
+        self.assertEqual(self.e.delete(test_key).prevValue, second_value)
+
+    def test_watch(self):
+        test_key = uuid.uuid4().hex
+        test_value = uuid.uuid4().hex
+        self.e.set(test_key, test_value, 1)
+        self.assertEquals(self.e.watch(test_key).action, "DELETE") 
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        host = sys.argv[1]
-    else:
-        host = "127.0.0.1"
-    e = Etcd(host)
-    print e.set("message", "hello world")
-    print e.set("message", "Hello World!")
-    print e.get("message")
-    try:
-        print e.get("nonexist")
-    except EtcdError as err:
-        print err
-    print e.delete("message")
-    try:
-        print e.delete("nonexist")
-    except EtcdError as err:
-        print err
-    print e.set("message", "HELLO WORLD!!", 5)
-    print e.watch("foo", timeout=2)
-    print e.watch("message")
-    e.set("message", "Goodbye, World")
-    print e.testandset("message", "Goodbye, World", "Hey Wait!")
-    try:
-        e.testandset("message", "Goodbye, World", "Try it again")
-    except EtcdError as err:
-        print err
-    print e.machines()
-    print e.leader()
+    unittest.main()
