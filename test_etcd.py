253ef98edbd3354b3a1e971c4c42c865e7f79c53
Bryan Berg <bryan@mixedmedialabs.com>
2013-09-06 20:21:53 -0700

    style and pep8!
    also, fixed an error when list() would bork in an error condition.

--- test_etcd.py
+++ test_etcd.py
@@ -3,11 +3,10 @@
 Test suite for etcd-py
 """
 
-import sys
 import uuid
 import unittest
 
-from etcd import *
+import etcd
 
 
 class TestEtcd(unittest.TestCase):
@@ -21,14 +20,14 @@ class TestEtcd(unittest.TestCase):
         self.assertEqual(self.e.get(test_key).value, first_value)
         second_value = uuid.uuid4().hex
         self.assertEqual(self.e.set(test_key, second_value).prevValue,
-                first_value)
+                         first_value)
         self.assertEqual(self.e.delete(test_key).prevValue, second_value)
 
     def test_watch(self):
         test_key = uuid.uuid4().hex
         test_value = uuid.uuid4().hex
         self.e.set(test_key, test_value, 1)
-        self.assertEquals(self.e.watch(test_key).action, "DELETE") 
+        self.assertEquals(self.e.watch(test_key).action, "DELETE")
 
 if __name__ == '__main__':
     unittest.main()
